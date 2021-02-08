e7a5be9394a7dfa94de43dc44808ebd4fbfdf5d5
Alessandro <alessandro.mpgh@gmail.com>
2021-02-08 17:07:35 -0300

    Added Functions.py in Python3

---
+++ functions.py
@@ -0,0 +1,19 @@
+def print_colors(color):
+    print(color)
+
+
+def generate_numbers(first, second):
+    numbers = range(first, second)
+    for n in numbers:
+        print(n)
+
+
+print_colors("red")
+print_colors("yellow")
+print_colors("blue")
+
+generate_numbers(0, 5)
+generate_numbers(5, 10)
+generate_numbers(10, 15)
+
+# 6. Writing a coin_flip Function Using Random
