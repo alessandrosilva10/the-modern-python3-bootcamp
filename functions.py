042fa89e57fe74b8d665846971988a2525a763e5
Alessandro <alessandro.mpgh@gmail.com>
2021-02-08 23:36:32 -0300

    global variable

--- functions.py
+++ functions.py
@@ -17,3 +17,57 @@ generate_numbers(5, 10)
 generate_numbers(10, 15)
 
 # 6. Writing a coin_flip Function Using Random
+
+from random import randrange
+
+
+def flip_coin():
+    if randrange(2) == 1:
+        return "Heads"
+    else:
+        return "Tails"
+
+
+print(flip_coin())
+
+
+def show_information(name="Alessandro", lastname="Silva"):
+    print(f'My name is {name} {lastname}')
+
+
+show_information()
+
+
+def add(a, b):
+    return a + b
+
+
+def math(a, b, fn=add):
+    print(fn(a, b))
+
+
+def subtract(a, b):
+    return a - b
+
+
+math(2, 1)
+math(2, 1, subtract)
+
+
+def exponent(num, pow):
+    return num ** pow
+
+
+print(exponent(pow=2, num=4))
+
+name = "Alessandro"
+print(name)
+
+
+def global_name():
+    global name
+    name = "Silva"
+
+
+global_name()
+print(name)
